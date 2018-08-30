2ccdfc6f438d69a91ca9ba6b3f3bfa0eb5b5dd44
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-08-30 16:27:38 -0300

    Connect to the ladder

---
+++ run.py
@@ -0,0 +1,22 @@
+import sc2, sys
+from __init__ import run_ladder_game
+from sc2 import Race, Difficulty
+from sc2.player import Bot, Computer
+
+# Load bot
+from ling_flood import EarlyAggro
+bot = Bot(Race.Zerg, EarlyAggro())
+
+# Start game
+if __name__ == '__main__':
+    if "--LadderServer" in sys.argv:
+        # Ladder game started by LadderManager
+        print("Starting ladder game...")
+        run_ladder_game(bot)
+    else:
+        # Local game
+        print("Starting local game...")
+        sc2.run_game(sc2.maps.get("Abyssal Reef LE"), [
+            bot,
+            Computer(Race.Protoss, Difficulty.VeryHard)
+        ], realtime=True)
