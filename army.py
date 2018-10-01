ea5d936d300b6a27a3139282c008b251880fe596
FW <tweakimp@gmail.com>
2018-10-01 03:59:31 +0200

    Reduce unit calls

--- army.py
+++ army.py
@@ -40,7 +40,7 @@ class army_control:
             filtered_enemies = self.known_enemy_units.not_structure.exclude_type(excluded_units)
             static_defence = self.known_enemy_units.of_type({SPINECRAWLER, PHOTONCANNON, BUNKER, PLANETARYFORTRESS})
             targets = static_defence | filtered_enemies.not_flying
-        atk_force = self.units(ZERGLING) | self.units(ULTRALISK)
+        atk_force = self.zerglings | self.ultralisks
         # enemy_detection = self.known_enemy_units.not_structure.of_type({OVERSEER, OBSERVER})
         for attacking_unit in atk_force:
             if targets and targets.closer_than(17, attacking_unit.position):
@@ -60,9 +60,9 @@ class army_control:
                 continue
             elif self.time < 1000 and not self.close_enemies_to_base:
                 if (
-                    len(self.units(ULTRALISK).ready) < 4
+                    len(self.ultralisks.ready) < 4
                     and self.supply_used not in range(198, 201)
-                    and len(self.units(ZERGLING).ready) < 41
+                    and len(self.zerglings.ready) < 41
                     and self.townhalls
                 ):
                     self.actions.append(
@@ -94,10 +94,10 @@ class army_control:
 
     async def queens_abilities(self):
         """Injection and creep spread"""
-        queens = self.units(QUEEN)
+        queens = self.queens
         hatchery = self.townhalls
         if hatchery:
-            # lowhp_ultralisks = self.units(ULTRALISK).filter(lambda lhpu: lhpu.health_percentage < 0.27)
+            # lowhp_ultralisks = self.ultralisks.filter(lambda lhpu: lhpu.health_percentage < 0.27)
             for queen in queens.idle:
                 # if not lowhp_ultralisks.closer_than(8, queen.position):
                 selected = hatchery.closest_to(queen.position)
@@ -120,7 +120,7 @@ class army_control:
     def scout_map(self):
         waypoints = [point for point in self.expansion_locations]
         start = self.start_location
-        scout = self.units(DRONE).closest_to(start)
+        scout = self.drones.closest_to(start)
         waypoints.sort(key=lambda p: ((p[0] - start[0]) ** 2 + (p[1] - start[1]) ** 2))
         for point in waypoints:
             self.actions.append(scout.move(point, queue=True))
