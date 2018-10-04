31c0d25dfe86eef113ea513b9b412569d4c7acaa
Sebastian Kraft <admin@helfull.de>
2018-10-04 18:16:47 +0200

    fix worker distribution

--- worker.py
+++ worker.py
@@ -120,7 +120,7 @@ class worker_control:
             elif difference < 0:
                     deficit_bases.append([mining_place, difference])
 
-        if deficit_bases:
+        if not deficit_bases:
             # no deficits so only move idle workers, not surplus and idle
             for drone in self.drones.idle:
                 if mineral_fields:
