1566ef68e5b7b0338f6874f934fb6a9e55f2d868
Matuiss2 <mtsbrbr@gmail.com>
2018-10-19 09:01:47 -0300

    Fixing last

--- actions/build/spines.py
+++ actions/build/spines.py
@@ -11,18 +11,17 @@ class BuildSpines:
 
     async def should_handle(self, iteration):
         """Requirements to run handle"""
-        if not self.ai.pools.ready:
+        if not self.ai.pools.ready and not self.ai.townhalls:
             return False
 
         return self.ai.close_enemy_production and len(self.ai.spines) < 4 and self.ai.already_pending(SPINECRAWLER) < 2
 
     async def handle(self, iteration):
         """Build the spines on the first base near the ramp in case there is a proxy"""
-        if self.ai.townhalls:
-            await self.ai.build(
-                SPINECRAWLER,
-                near=self.ai.townhalls.furthest_to(self.ai._game_info.map_center).position.towards(
-                    self.ai.main_base_ramp.depot_in_middle, 14
-                ),
-            )
-            return True
+        await self.ai.build(
+            SPINECRAWLER,
+            near=self.ai.townhalls.furthest_to(self.ai._game_info.map_center).position.towards(
+                self.ai.main_base_ramp.depot_in_middle, 14
+            ),
+        )
+        return True
