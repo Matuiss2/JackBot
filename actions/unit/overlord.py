1e851679dcc879302608eb160ea17773e2626bd1
Matuiss2 <mtsbrbr@gmail.com>
2018-10-26 23:31:14 -0300

    changing overlord scouting logic

--- actions/unit/overlord.py
+++ actions/unit/overlord.py
@@ -20,17 +20,10 @@ class Overlord:
         local_controller = self.ai
         action = local_controller.add_action
         map_center = local_controller._game_info.map_center
-        # enemy_main = local_controller.enemy_start_locations[0]  # point2
-        # enemy_natural = min(
-        #    local_controller.ordered_expansions,
-        #    key=lambda expo: (expo.x - enemy_main.x) ** 2 + (expo.y - enemy_main.y) ** 2
-        #    if expo.x - enemy_main.x != 0 and expo.y - enemy_main.y != 0
-        #   else 10 ** 10,
-        # )
         if not self.first_ov_scout:
             self.first_ov_scout = True
             action(
-                local_controller.overlords.first.move(local_controller.ordered_expansions[1].towards(map_center, 18))
+                local_controller.overlords.first.move(local_controller.ordered_expansions[-2].towards(map_center, -15))
             )
         elif not self.second_ov_scout and len(local_controller.overlords.ready) == 2:
             second_ov = local_controller.overlords.ready.closest_to(local_controller.townhalls.first)
@@ -40,4 +33,5 @@ class Overlord:
         elif self.second_ov_scout and not self.third_ov_scout and len(local_controller.overlords.ready) == 3:
             self.third_ov_scout = True
             third_ov = local_controller.overlords.ready.closest_to(local_controller.townhalls.furthest_to(map_center))
-            action(third_ov.move(map_center))
+
+            action(third_ov.move(local_controller.ordered_expansions[1].towards(map_center, 18)))
