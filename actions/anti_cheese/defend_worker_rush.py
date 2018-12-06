2e2599ff428ecbcbee7679822223de712955ad71
Matuiss2 <mtsbrbr@gmail.com>
2018-12-06 01:09:05 -0200

    Remove functions for non-repeated 1 liners, group repeated values.

--- actions/anti_cheese/defend_worker_rush.py
+++ actions/anti_cheese/defend_worker_rush.py
@@ -22,33 +22,28 @@ class DefendWorkerRush(Micro):
         self.base = local_controller.hatcheries.ready
         if not self.base:
             return False
-
         self.enemy_units_close = local_controller.enemies.closer_than(8, self.base.first).of_type({PROBE, DRONE, SCV})
-        return (
-            self.enemy_units_close
-            and not self.defender_tags
-            or self.defender_tags
-            and not self.enemy_units_close
-            or self.defender_tags
-            and self.enemy_units_close
-        )
+        return self.enemy_units_close or self.defender_tags
 
     async def handle(self, iteration):
         """It destroys every worker rush without losing more than 2 workers,
          it counter scouting worker rightfully now, its too big and can be split"""
-        if self.enemy_units_close and not self.defender_tags:
-            self.build_defense_force(len(self.enemy_units_close))
-        if self.defender_tags and not self.enemy_units_close:
-            self.clear_defense_force(self.base)
-        if self.defender_tags and self.enemy_units_close:
-            self.refill_defense_force(len(self.enemy_units_close))
-            for drone in self.defenders:
-                # 6 hp is the lowest you can take a hit and still survive
-                if not self.save_lowhp_drone(drone, self.base):
-                    if drone.weapon_cooldown <= 0.60 * 22.4:
-                        self.attack_close_target(drone, self.enemy_units_close)
-                    elif not self.move_to_next_target(drone, self.enemy_units_close):
-                        self.move_lowhp(drone, self.enemy_units_close)
+        close_workers = self.enemy_units_close
+        enemy_worker_force = len(close_workers)
+        if self.defender_tags:
+            base = self.base
+            if close_workers:
+                self.refill_defense_force(enemy_worker_force)
+                for drone in self.defenders:
+                    if not self.save_lowhp_drone(drone, base):
+                        if drone.weapon_cooldown <= 0.60 * 22.4:
+                            self.attack_close_target(drone, close_workers)
+                        elif not self.move_to_next_target(drone, close_workers):
+                            self.move_lowhp(drone, close_workers)
+            else:
+                self.clear_defense_force(base)
+        elif close_workers:
+            self.defender_tags = self.defense_force(enemy_worker_force * 2)
 
     def save_lowhp_drone(self, drone, base):
         """Remove drones with less 6 hp from the defending force"""
@@ -63,14 +58,10 @@ class DefendWorkerRush(Micro):
             return True
         return False
 
-    def build_defense_force(self, enemy_count):
-        """Finds the right amount for the defense force(max twice of the attacker number ideally)"""
-        self.defender_tags = self.defense_force(enemy_count + enemy_count)
-
     def refill_defense_force(self, enemy_count):
         """If there is less workers on the defenders force than the ideal refill it"""
         self.defenders = self.ai.drones.filter(lambda worker: worker.tag in self.defender_tags and worker.health > 0)
-        defender_deficit = self.calculate_defender_deficit(enemy_count)
+        defender_deficit = min(len(self.ai.drones) - 1, enemy_count + enemy_count) - len(self.defenders)
         if defender_deficit > 0:
             additional_drones = self.defense_force(defender_deficit)
             self.defender_tags = self.defender_tags + additional_drones
@@ -78,20 +69,11 @@ class DefendWorkerRush(Micro):
     def clear_defense_force(self, base):
         """If there is more workers on the defenders force than the ideal put it back to mining"""
         local_controller = self.ai
-        if self.defenders:
-            for drone in self.defenders:
-                local_controller.add_action(drone.gather(local_controller.state.mineral_field.closest_to(base.first)))
+        for drone in self.defenders:
+            local_controller.add_action(drone.gather(local_controller.state.mineral_field.closest_to(base.first)))
         self.defender_tags = []
         self.defenders = None
 
     def defense_force(self, count):
-        """Put all drones needed on the defenders force - order based on health"""
-        return [unit.tag for unit in self.highest_hp_drones(count)]
-
-    def highest_hp_drones(self, count):
-        """Order the drones based on health(highest first)"""
-        return heapq.nlargest(count, self.ai.drones.collecting, key=lambda drones: drones.health)
-
-    def calculate_defender_deficit(self, enemy_count):
-        """Calculates the deficit on the defense force"""
-        return min(len(self.ai.drones) - 1, enemy_count + enemy_count) - len(self.defenders)
+        """Put all drones needed on the defenders force - Order the drones based on health(highest first)"""
+        return [unit.tag for unit in heapq.nlargest(count, self.ai.drones.collecting, key=lambda drones: drones.health)]
