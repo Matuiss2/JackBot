9ff5ca862524b4e4957d10dbfda6b3b8e94794d5
Matuiss2 <mtsbrbr@gmail.com>
2019-01-19 00:22:09 -0200

    remove internal module(upkeep was becoming impossible), use main one now

--- sc2/constants.py
+++
@@ -1,6 +0,0 @@
-"""Groups the constants from sc2"""
-from .ids.ability_id import *
-from .ids.buff_id import *
-from .ids.effect_id import *
-from .ids.unit_typeid import *
-from .ids.upgrade_id import *
