bd3189818a8cb385ddda37738e9cda29ab15c3c4
Matuiss2 <mtsbrbr@gmail.com>
2019-01-27 00:30:19 -0200

    Fix or at least reduce the hydra micro bug

--- actions/micro/micro_main.py
+++ actions/micro/micro_main.py
@@ -126,8 +126,7 @@ class ArmyControl(ZerglingControl, HydraControl, Micro, EnemyArmyValue):
         """Control the idle units, by gathering then or telling then to attack"""
         local_controller = self.controller
         if (
-            local_controller.supply_used not in range(198, 201)
-            and self.gathering_force_value(1, 2, 4) < 39 + 1.25 * local_controller.time // 100
+            self.gathering_force_value(1, 2, 4) < 42
             and local_controller.townhalls
             and self.retreat_units
             and not local_controller.counter_attack_vs_flying
