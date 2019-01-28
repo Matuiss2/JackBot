cb6830b228b86cd153d4f4c133b09939564b8264
Matuiss2 <mtsbrbr@gmail.com>
2019-01-28 00:21:51 -0200

    fix realtime=true bug, hopefully

--- main.py
+++ main.py
@@ -115,6 +115,8 @@ class JackBot(sc2.BotAI, MainDataContainer, CreepControl, BuildingPositioning, B
 
     async def on_step(self, iteration):
         """Group all other functions in this bot, its the main"""
+        if self.iteration == iteration:
+            return None
         self.iteration = iteration
         self.prepare_data()
         self.set_game_step()
