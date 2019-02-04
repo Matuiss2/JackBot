f9b93cd3ca21da46b828d675b6e43149442bab17
Matuiss2 <mtsbrbr@gmail.com>
2019-02-04 17:55:48 -0200

    globalize repeated variables part6

--- data_containers/our_stuff.py
+++ data_containers/our_stuff.py
@@ -3,23 +3,23 @@ from sc2.constants import HATCHERY, HIVE, LAIR
 
 from data_containers.our_structures import OurBuildingsData
 from data_containers.our_units import OurUnitsData
+from data_containers.quantity_data import OurQuantityData
 
 
-class OurStuffData(OurBuildingsData, OurUnitsData):
+class OurStuffData(OurBuildingsData, OurUnitsData, OurQuantityData):
     """This is the data container for all our units and buildings"""
 
     def __init__(self):
         OurBuildingsData.__init__(self)
         OurUnitsData.__init__(self)
-        self.structures = self.hatcheries = self.lairs = self.hives = self.base_amount = self.hydra_amount = None
-        self.zergling_amount = self.drone_amount = self.overlord_amount = None
+        self.structures = self.hatcheries = self.lairs = self.hives = None
 
     def initialize_our_stuff(self):
         """Initializes our stuff"""
         self.initialize_units()
         self.initialize_buildings()
         self.initialize_bases()
-        self.initialize_amounts()
+        self.initialize_all_amounts()
 
     def initialize_units(self):
         """Initialize our units"""
@@ -40,9 +40,6 @@ class OurStuffData(OurBuildingsData, OurUnitsData):
         self.lairs = self.units(LAIR)
         self.hives = self.units(HIVE)
 
-    def initialize_amounts(self):
-        self.base_amount = len(self.townhalls)
-        self.hydra_amount = len(self.hydras)
-        self.zergling_amount = len(self.zerglings)
-        self.drone_amount = len(self.drones)
-        self.overlord_amount = len(self.overlords)
+    def initialize_all_amounts(self):
+        self.initialize_building_amounts()
+        self.initialize_unit_amounts()
---
+++ data_containers/quantity_data.py
@@ -0,0 +1,17 @@
+"""All data that are related to quantity of the stuff on our possession are here"""
+
+
+class OurQuantityData:
+    """This is the data container for all our stuff amount"""
+
+    def __init__(self):
+        self.hydra_amount = self.zergling_amount = self.drone_amount = self.overlord_amount = self.base_amount = None
+
+    def initialize_building_amounts(self):
+        self.base_amount = len(self.townhalls)
+
+    def initialize_unit_amounts(self):
+        self.hydra_amount = len(self.hydras)
+        self.zergling_amount = len(self.zerglings)
+        self.drone_amount = len(self.drones)
+        self.overlord_amount = len(self.overlords)
