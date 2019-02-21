fef7e8c4816c412a7fcb964e91c6ee94f4cdc0d0
Matuiss2 <mtsbrbr@gmail.com>
2019-02-21 17:15:00 -0300

    Only initial base scout on 4 player maps(no point on the early base scout since it still don't have a anti-rush implementation)

--- run.py
+++ run.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
         # Local game
         print("Starting local game...")
         RANDOM_MAP = random.choice(["BlueshiftLE", "KairosJunctionLE", "ParaSiteLE", "PortAleksanderLE"])
-        # 797 - 203 / 782 - 218 /
-        sc2.run_game(sc2.maps.get(RANDOM_MAP), [BOT, Computer(Race.Terran, Difficulty.CheatVision)], realtime=False)
+        # 797 - 203 / 782 - 218 / 795 - 205
+        sc2.run_game(sc2.maps.get(RANDOM_MAP), [BOT, Computer(Race.Protoss, Difficulty.CheatVision)], realtime=False)
         # sc2.run_game(sc2.maps.get("drone_worker_defense"), [bot], realtime=True)
         # sc2.run_game(sc2.maps.get("drone_scout_defense"), [bot], realtime=True)
         # sc2.run_game(sc2.maps.get("test_anti_colossus"),[bot, Computer(Race.Protoss,
