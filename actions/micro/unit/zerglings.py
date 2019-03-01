7d8f0b776259becf5b94de9420da8f856686be8d
Matuiss2 <mtsbrbr@gmail.com>
2019-03-01 12:28:41 -0300

    Improve the docs following numpy recommendation style (for methods with parameters and returns both) part 2

--- actions/micro/unit/zerglings.py
+++ actions/micro/unit/zerglings.py
@@ -7,6 +7,16 @@ class ZerglingControl(Micro):
     """Can be improved in many ways"""
 
     def baneling_dodge(self, unit, targets):
+        """
+        Parameters
+        ----------
+        unit: One zergling from the attacking force
+        targets: The enemy zergling targets, it groups almost every enemy unit on the ground
+
+        Returns
+        -------
+        Chosen action when a baneling is near(attack, move away or pop the baneling)
+        """
         """If the enemy has banelings, run baneling dodging code. It can be improved,
          its a little bugged and just avoid the baneling not pop it"""
         self.handling_anti_banelings_group()
@@ -30,14 +40,34 @@ class ZerglingControl(Micro):
         return False
 
     def baneling_group(self, unit, targets):
-        """Put the banelings on one group"""
+        """
+        Put the banelings on one group
+        Parameters
+        ----------
+        unit: One zergling from the attacking force
+        targets: The enemy zergling targets, it groups almost every enemy unit on the ground
+
+        Returns
+        -------
+        A group that includes every close baneling
+        """
         threats = self.trigger_threats(targets, unit, 5)
         for threat in threats:
             if threat.type_id == BANELING:
                 yield threat
 
     def baneling_trigger(self, unit, baneling):
-        """If we haven't triggered any banelings, trigger it."""
+        """
+        If we haven't triggered any banelings, trigger it.
+        Parameters
+        ----------
+        unit: One zergling from the attacking force
+        baneling: A baneling from the baneling_group
+
+        Returns
+        -------
+        Action to pop the enemy baneling
+        """
         self.baneling_sacrifices[unit] = baneling
         self.main.add_action(unit.attack(baneling))
         return True
@@ -49,7 +79,17 @@ class ZerglingControl(Micro):
                 del self.baneling_sacrifices[zergling]
 
     def micro_zerglings(self, unit, targets):
-        """Target low hp units smartly, and surrounds when attack cd is down"""
+        """
+        Target low hp units smartly, and surrounds when attack cd is down
+        Parameters
+        ----------
+        unit: One zergling from the attacking force
+        targets: The enemy zergling targets, it groups almost every enemy unit on the ground
+
+        Returns
+        -------
+        The chosen action depending on the zergling situation
+        """
         if self.baneling_dodge(unit, targets):
             return True
         if self.zergling_modifiers(unit, targets):
@@ -60,7 +100,16 @@ class ZerglingControl(Micro):
         return True
 
     def zergling_modifiers(self, unit, targets):
-        """Modifiers for zerglings"""
+        """
+        Modifiers for zerglings
+        Parameters
+        ----------
+        unit: One zergling from the attacking force
+        targets: The enemy zergling targets, it groups almost every enemy unit on the ground
+        Returns
+        -------
+        The chosen action depending on the modifiers
+        """
         if unit.weapon_cooldown <= 8.85 or (
             unit.weapon_cooldown <= 6.35 and self.main.already_pending_upgrade(ZERGLINGATTACKSPEED) == 1
         ):
