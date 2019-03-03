defb342ce06ad77db90e994f34898ae202477b06
Matuiss2 <mtsbrbr@gmail.com>
2019-03-03 02:43:28 -0300

    scout always

--- actions/micro/unit/drone.py
+++ actions/micro/unit/drone.py
@@ -16,7 +16,7 @@ class Drone:
         """It sends 2 drones to scout the map, for bases(only if its a more than 2 players map) or proxies"""
         for point in self.main.ordered_expansions[1:6]:
             self.main.add_action(self.main.drones.closest_to(self.main.start_location).move(point, queue=True))
-        if len(self.main.enemy_start_locations) > 1 and not self.rush_scout:
+        if not self.rush_scout:
             self.rush_scout = True
             for point in self.main.enemy_start_locations:
                 self.main.add_action(self.main.drones.random.move(point, queue=True))
