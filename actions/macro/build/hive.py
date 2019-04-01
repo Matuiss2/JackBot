dbc4ca6cb77f27494a31ba54917459fe4977b12a
Matuiss2 <mtsbrbr@gmail.com>
2019-04-01 11:31:30 -0300

    BotWithAPlan 90% part7 currently 70-5(fix - cannot find reference(false but annoying flake8 error) on hive)

--- actions/macro/build/hive.py
+++ actions/macro/build/hive.py
@@ -1,5 +1,5 @@
 """Everything related to building logic for the hives goes here"""
-from sc2.constants import HIVE, UPGRADETOHIVE_HIVE
+from sc2.constants import AbilityId, UnitTypeId
 
 
 class BuildHive:
@@ -13,9 +13,9 @@ class BuildHive:
         """Requirement to build the hive, maybe its too greedy maybe we should raise the lock for it"""
         self.selected_lairs = self.main.lairs.ready.idle
         return self.selected_lairs and self.main.can_build_unique(
-            HIVE, self.main.caverns, self.main.pits.ready, all_units=True
+            UnitTypeId.HIVE, self.main.caverns, self.main.pits.ready, all_units=True
         )
 
     async def handle(self):
         """Finishes the action of making the hive"""
-        self.main.add_action(self.selected_lairs.first(UPGRADETOHIVE_HIVE))
+        self.main.add_action(self.selected_lairs.first(AbilityId.UPGRADETOHIVE_HIVE))
