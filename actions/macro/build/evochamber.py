4c8af8d94888c44e2974f7d502676db1e0f332dd
Matuiss2 <mtsbrbr@gmail.com>
2019-04-01 04:42:18 -0300

    BotWithAPlan 90% part6 currently 65-5(some tweaks on mineral overflow for zerglings, simplify and fix - cannot find reference(false but annoying flake8 error) on evochamber building and upgrades)

--- actions/macro/build/evochamber.py
+++ actions/macro/build/evochamber.py
@@ -1,5 +1,5 @@
 """Everything related to building logic for the evolution chamber goes here"""
-from sc2.constants import EVOLUTIONCHAMBER
+from sc2.constants import UnitTypeId
 
 
 class BuildEvochamber:
@@ -11,13 +11,12 @@ class BuildEvochamber:
     async def should_handle(self):
         """Requirements for building the evolution chambers, maybe its to early can probably be improved"""
         if (
-            self.main.building_requirement(EVOLUTIONCHAMBER, self.main.pools.ready, one_at_time=True)
+            self.main.building_requirement(UnitTypeId.EVOLUTIONCHAMBER, self.main.pools.ready, one_at_time=True)
             and len(self.main.evochambers) < 2
         ):
-            if not self.main.evochambers.ready:
-                return self.main.base_amount >= 3
-            return self.main.ready_base_amount >= 3
+
+            return self.main.base_amount >= 3 if not self.main.evochambers.ready else self.main.ready_base_amount >= 3
 
     async def handle(self):
         """Build the evochamber"""
-        await self.main.place_building(EVOLUTIONCHAMBER)
+        await self.main.place_building(UnitTypeId.EVOLUTIONCHAMBER)
