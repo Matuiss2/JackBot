afdec0f2b0e2d54618e89a4dcbebce57f2e4016b
Matuiss2 <mtsbrbr@gmail.com>
2019-04-01 09:22:00 -0300

    BotWithAPlan 90% part7 currently 70-5(fix - cannot find reference(false but annoying flake8 error) on creep_spread)

--- actions/macro/build/creep_spread.py
+++ actions/macro/build/creep_spread.py
@@ -1,6 +1,6 @@
 """Everything related to calculate the creep spreading goes here"""
 import math
-from sc2.constants import BUILD_CREEPTUMOR_QUEEN, BUILD_CREEPTUMOR_TUMOR, ZERGBUILD_CREEPTUMOR
+from sc2.constants import AbilityId
 from sc2.data import ActionResult
 from sc2.position import Point2
 
@@ -23,10 +23,10 @@ class CreepControl:
         Makes creep to the enemy base, needs a better value function for the spreading, it gets stuck on ramps"""
         # Make sure unit can make tumor and what ability it is
         abilities = await self.get_available_abilities(unit)
-        if BUILD_CREEPTUMOR_QUEEN in abilities:
-            self.unit_ability = BUILD_CREEPTUMOR_QUEEN
-        elif BUILD_CREEPTUMOR_TUMOR in abilities:
-            self.unit_ability = BUILD_CREEPTUMOR_TUMOR
+        if AbilityId.BUILD_CREEPTUMOR_QUEEN in abilities:
+            self.unit_ability = AbilityId.BUILD_CREEPTUMOR_QUEEN
+        elif AbilityId.BUILD_CREEPTUMOR_TUMOR in abilities:
+            self.unit_ability = AbilityId.BUILD_CREEPTUMOR_TUMOR
         else:
             return None
         # defining vars
@@ -45,7 +45,7 @@ class CreepControl:
         ]
         # check if any of the positions are valid
         creep_mask = await self._client.query_building_placement(
-            self._game_data.abilities[ZERGBUILD_CREEPTUMOR.value], positions
+            self._game_data.abilities[AbilityId.ZERGBUILD_CREEPTUMOR.value], positions
         )
         # filter valid results
         valid_placements = [p for idx, p in enumerate(positions) if creep_mask[idx] == ActionResult.Success]
@@ -60,14 +60,14 @@ class CreepControl:
             else:
                 self.valid_placements = sorted(valid_placements, key=lambda pos: pos.distance_to_point2(final_destiny))
             self.avoid_blocking_expansions(unit)
-            if self.unit_ability == BUILD_CREEPTUMOR_TUMOR:
+            if self.unit_ability == AbilityId.BUILD_CREEPTUMOR_TUMOR:
                 self.used_tumors.append(unit.tag)
 
     def avoid_blocking_expansions(self, unit):
         """ This is very expensive to the cpu, need optimization, keeps creep outside expansion locations"""
         for c_location in self.valid_placements:
             if all(c_location.distance_to_point2(el) > 8.5 for el in self.expansion_locations):
-                if self.unit_ability == BUILD_CREEPTUMOR_QUEEN or not self.tumors:
+                if self.unit_ability == AbilityId.BUILD_CREEPTUMOR_QUEEN or not self.tumors:
                     self.add_action(unit(self.unit_ability, c_location))
                     break
                 if c_location.distance_to_closest(self.tumors) >= 4:
