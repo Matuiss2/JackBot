9b316cf962ad49f80c3911e5c1627423242ac81a
Matuiss2 <mtsbrbr@gmail.com>
2019-04-02 14:57:19 -0300

    BotWithAPlan 90% part7 currently 75-5(fix - cannot find reference(false but annoying flake8 error) on zergling)

--- actions/macro/train/zergling.py
+++ actions/macro/train/zergling.py
@@ -1,5 +1,5 @@
 """Everything related to training zergling goes here"""
-from sc2.constants import ZERGLING, ZERGLINGMOVEMENTSPEED
+from sc2.constants import UnitTypeId, UpgradeId
 
 
 class TrainZergling:
@@ -12,7 +12,7 @@ class TrainZergling:
         """Requirements to train zerglings, good enough for now but ratio values can probably be improved"""
         if self.upgrades_lock():
             return False
-        if not self.main.can_train(ZERGLING, self.main.pools.ready):
+        if not self.main.can_train(UnitTypeId.ZERGLING, self.main.pools.ready):
             return False
         if self.main.minerals >= 600 and self.main.ready_base_amount <= 5:
             return True
@@ -26,12 +26,12 @@ class TrainZergling:
 
     async def handle(self):
         """Execute the action of training zerglings"""
-        self.main.add_action(self.main.larvae.random.train(ZERGLING))
+        self.main.add_action(self.main.larvae.random.train(UnitTypeId.ZERGLING))
 
     def upgrades_lock(self):
         """ Don't make zerglings if the zergling speed isn't done yet after 2:25"""
         return (
-            not self.main.already_pending_upgrade(ZERGLINGMOVEMENTSPEED)
+            not self.main.already_pending_upgrade(UpgradeId.ZERGLINGMOVEMENTSPEED)
             and self.main.time < 145
             and not self.main.close_enemy_production
         )
