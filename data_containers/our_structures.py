6dcc13254b7d30d8cf5860c75282515f25902961
Matuiss2 <mtsbrbr@gmail.com>
2019-04-04 03:25:29 -0300

    BotWithAPlan 90% part8 currently 85-5(Predefine all list sets or tuples on the constructors on our_structures)The main reason for this is organization and to save some ugly line breaks

--- data_containers/our_structures.py
+++ data_containers/our_structures.py
@@ -8,15 +8,14 @@ class OurBuildingsData:
     def __init__(self):
         self.evochambers = self.pools = self.spines = self.tumors = self.extractors = self.spores = self.spires = None
         self.hydradens = self.pits = self.caverns = None
+        self.creep_types = {UnitTypeId.CREEPTUMORQUEEN, UnitTypeId.CREEPTUMOR, UnitTypeId.CREEPTUMORBURROWED}
 
     def initialize_hatchery_buildings(self):
         """Initialize all our buildings(hatchery tech)"""
         self.evochambers = self.units(UnitTypeId.EVOLUTIONCHAMBER)
         self.pools = self.units(UnitTypeId.SPAWNINGPOOL)
         self.spines = self.units(UnitTypeId.SPINECRAWLER)
-        self.tumors = self.units.of_type(
-            {UnitTypeId.CREEPTUMORQUEEN, UnitTypeId.CREEPTUMOR, UnitTypeId.CREEPTUMORBURROWED}
-        )
+        self.tumors = self.units.of_type(self.creep_types)
         self.extractors = self.units(UnitTypeId.EXTRACTOR)
         self.spores = self.units(UnitTypeId.SPORECRAWLER)
         self.spires = self.units(UnitTypeId.SPIRE)
