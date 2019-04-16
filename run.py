0404a14c90c29c6c3a182c4be2f8ada8be62c5eb
Matuiss2 <mtsbrbr@gmail.com>
2019-04-16 04:55:01 -0300

    BotWithAPlan 90% part13 currently 114-6(reduce game-steps)It improves the strength by an ok amount

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import random
 import sys
 import sc2
-from sc2 import Race, Difficulty
+from sc2 import Race, Difficulty  # AIBuild
 from sc2.player import Bot, Computer
 from __init__ import run_ladder_game
 from main import JackBot
@@ -16,7 +16,10 @@ if __name__ == "__main__":
     else:
         # Local game
         print("Starting local game...")
-        RANDOM_MAP = random.choice(["BlueshiftLE", "KairosJunctionLE", "ParaSiteLE", "PortAleksanderLE"])
+        RANDOM_MAP = random.choice(("BlueshiftLE", "KairosJunctionLE", "ParaSiteLE", "PortAleksanderLE"))
+        # RANDOM_BUILD = random.choice(
+        # (sc2.AIBuild.Rush, sc2.AIBuild.Timing, sc2.AIBuild.Power, sc2.AIBuild.Macro, sc2.AIBuild.Air)
+        # )
         # 797 - 203 / 782 - 218 / 795 - 205 / 820 - 180
         # 658 - 342 / 692 - 308 /
         sc2.run_game(sc2.maps.get(RANDOM_MAP), [BOT, Computer(Race.Protoss, Difficulty.CheatMoney)], realtime=False)
