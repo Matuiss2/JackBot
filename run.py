200d88a56b3e154082151e7028253e9256d2c060
Matuiss2 <mtsbrbr@gmail.com>
2019-05-13 23:22:39 -0300

    implement AI builds

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import random
 import sys
 import sc2
-from sc2 import Race, Difficulty  # AIBuild
+from sc2 import Race, Difficulty, AIBuild
 from sc2.player import Bot, Computer
 from __init__ import run_ladder_game
 from main import JackBot
@@ -17,9 +17,11 @@ if __name__ == "__main__":
         # Local game
         print("Starting local game...")
         RANDOM_MAP = random.choice(("BlueshiftLE", "KairosJunctionLE", "ParaSiteLE", "PortAleksanderLE"))
-        # RANDOM_BUILD = random.choice(
-        # (sc2.AIBuild.Rush, sc2.AIBuild.Timing, sc2.AIBuild.Power, sc2.AIBuild.Macro, sc2.AIBuild.Air)
-        # )
+        RANDOM_BUILD = random.choice(
+            (sc2.AIBuild.Rush, sc2.AIBuild.Timing, sc2.AIBuild.Power, sc2.AIBuild.Macro, sc2.AIBuild.Air)
+        )
         # 797 - 203 / 782 - 218 / 795 - 205 / 820 - 180
         # 658 - 342 / 692 - 308 /
-        sc2.run_game(sc2.maps.get(RANDOM_MAP), [BOT, Computer(Race.Terran, Difficulty.CheatMoney)], realtime=False)
+        sc2.run_game(
+            sc2.maps.get(RANDOM_MAP), [BOT, Computer(Race.Terran, Difficulty.CheatMoney, RANDOM_BUILD)], realtime=False
+        )
