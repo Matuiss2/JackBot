040a6c42c9f34926e75714c64b27829ab324ff6c
Matuiss2 <mtsbrbr@gmail.com>
2019-05-13 22:15:29 -0300

    remove unused stuff

--- data_containers/our_units.py
+++ data_containers/our_units.py
@@ -8,18 +8,13 @@ class OurUnitsData:
     def __init__(self):
         self.overlords = self.drones = self.queens = self.zerglings = self.larvae = self.overseers = None
         self.mutalisks = self.hydras = self.ultralisks = None
-        self.burrowed_lings = []
 
     def initialize_hatchery_units(self):
         """Initialize all our buildings(hatchery tech)"""
         self.overlords = self.units(UnitTypeId.OVERLORD)
         self.drones = self.units(UnitTypeId.DRONE)
         self.queens = self.units(UnitTypeId.QUEEN)
-        self.zerglings = (
-            self.units(UnitTypeId.ZERGLING).tags_not_in(self.burrowed_lings)
-            if self.burrowed_lings
-            else self.units(UnitTypeId.ZERGLING)
-        )
+        self.zerglings = self.units(UnitTypeId.ZERGLING)
         self.larvae = self.units(UnitTypeId.LARVA)
 
     def initialize_hive_units(self):
