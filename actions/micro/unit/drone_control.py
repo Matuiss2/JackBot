1cfc83da1e08fb9bfefbb2ca6e2d4e2b46a4db48
Matuiss2 <mtsbrbr@gmail.com>
2019-06-10 04:05:16 -0300

    Simplify drone control

--- actions/micro/unit/drone_control.py
+++ actions/micro/unit/drone_control.py
@@ -6,17 +6,16 @@ class DroneControl:
 
     def __init__(self, main):
         self.main = main
-        self.rush_scout = self.proxy_scout = False
+        self.scout = False
 
     async def should_handle(self):
         """Sends the scouting drone on the beginning"""
-        return not (self.rush_scout and self.proxy_scout)
+        return not self.scout
 
     async def handle(self):
         """It sends a drone to scout the map, for bases or proxies"""
         selected_drone = self.main.drones.random
-        self.rush_scout = True
-        self.proxy_scout = True
+        self.scout = True
         for point in self.main.ordered_expansions[1:6]:
             self.main.add_action(selected_drone.move(point, queue=True))
         for point in self.main.enemy_start_locations:
