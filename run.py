37746f4a4ad776e16a1fd9abdec545e3f6a66d69
Matuiss2 <mtsbrbr@gmail.com>
2019-07-06 13:33:51 -0300

    Don't test more than 10 of same set

--- run.py
+++ run.py
@@ -13,11 +13,15 @@ if __name__ == "__main__":
         run_ladder_game(Bot(Race.Zerg, JackBot()))
     else:
         # Local game
-        print("Starting local game...")
-        MAP = random.choice(["BlueshiftLE", "KairosJunctionLE", "ParaSiteLE", "PortAleksanderLE"])
-        BUILD = random.choice([AIBuild.Rush, AIBuild.Timing, AIBuild.Power, AIBuild.Macro, AIBuild.Air])
-        DIFFICULTY = random.choice([Difficulty.CheatVision, Difficulty.CheatMoney, Difficulty.CheatInsane])
-        RACE = random.choice([Race.Zerg, Race.Terran, Race.Protoss])
+        while True:
+            MAP = random.choice(["BlueshiftLE", "KairosJunctionLE", "ParaSiteLE", "PortAleksanderLE"])
+            BUILD = random.choice([AIBuild.Rush, AIBuild.Timing, AIBuild.Power, AIBuild.Macro, AIBuild.Air])
+            DIFFICULTY = random.choice([Difficulty.CheatVision, Difficulty.CheatMoney, Difficulty.CheatInsane])
+            RACE = random.choice([Race.Zerg, Race.Terran, Race.Protoss])
+            if BUILD == AIBuild.Timing and DIFFICULTY == Difficulty.CheatInsane and RACE == Race.Protoss:
+                print(f"\n{DIFFICULTY.name} {RACE.name} {BUILD.name} already done")
+                continue
+            break
         print(f"\n{DIFFICULTY.name} {RACE.name} {BUILD.name}")
         BOT = Bot(Race.Zerg, JackBot())
         BUILTIN_BOT = Computer(RACE, DIFFICULTY, BUILD)
