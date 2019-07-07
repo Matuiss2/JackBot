0ab8f23f66433f678850f6e21e1eff29de8f64d7
Matuiss2 <mtsbrbr@gmail.com>
2019-07-07 19:36:14 -0300

    Try to avoid if checks on handle() - put all checks that we can on should_handle() on methods that are part of should_handle()- part 2

--- actions/macro/build/spine_construction.py
+++ actions/macro/build/spine_construction.py
@@ -7,6 +7,7 @@ class SpineConstruction:
 
     def __init__(self, main):
         self.main = main
+        self.placement_position = None
 
     async def should_handle(self):
         """Requirements to build the spines"""
@@ -16,10 +17,13 @@ class SpineConstruction:
             and self.main.building_requirement(UnitTypeId.SPINECRAWLER, self.main.pools.ready)
             and self.main.townhalls
         ):
-            return self.main.close_enemy_production or self.main.close_enemies_to_base
+            self.placement_position = self.main.furthest_townhall_to_center.position.towards(
+                self.main.main_base_ramp.depot_in_middle, 14
+            )
+            return (self.main.close_enemy_production or self.main.close_enemies_to_base) and not any(
+                enemy.distance_to(self.placement_position) < 13 for enemy in self.main.enemies
+            )
 
     async def handle(self):
         """Build the spines on the first base near the ramp in case there is a proxy"""
-        position = self.main.furthest_townhall_to_center.position.towards(self.main.main_base_ramp.depot_in_middle, 14)
-        if not any(enemy.distance_to(position) < 13 for enemy in self.main.enemies):
-            await self.main.build(UnitTypeId.SPINECRAWLER, near=position)
+        await self.main.build(UnitTypeId.SPINECRAWLER, near=self.placement_position)
