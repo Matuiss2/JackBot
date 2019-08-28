b8cd887d7a63dea93b88069d551d351eb2a620f9
Matuiss2 <mtsbrbr@gmail.com>
2019-08-28 13:36:19 -0300

    improving naming part9

--- actions/macro/buildings_positions.py
+++ actions/macro/buildings_positions.py
@@ -14,10 +14,10 @@ class BuildingsPositions:
         """See if its possible to build an evochamber or an engineering bay at the position - checking both is needed
         because it runs at the main base that has creep already(evochamber can be placed) but it also runs on new
         bases as well and on this ones the creep doesn't exist at the position yet(engineering bay can be placed)"""
-        e_bay_ability = self._game_data.units[UnitTypeId.ENGINEERINGBAY.value].creation_ability
-        e_bay_mask = await self._client.query_building_placement(e_bay_ability, self.viable_points)
-        evo_ability = self._game_data.units[UnitTypeId.EVOLUTIONCHAMBER.value].creation_ability
-        evo_mask = await self._client.query_building_placement(evo_ability, self.viable_points)
+        e_bay_creation_ability = self._game_data.units[UnitTypeId.ENGINEERINGBAY.value].creation_ability
+        e_bay_mask = await self._client.query_building_placement(e_bay_creation_ability, self.viable_points)
+        evo_creation_ability = self._game_data.units[UnitTypeId.EVOLUTIONCHAMBER.value].creation_ability
+        evo_mask = await self._client.query_building_placement(evo_creation_ability, self.viable_points)
         for point in [
             point
             for i, point in enumerate(self.viable_points)
@@ -39,20 +39,20 @@ class BuildingsPositions:
                 return building_position
         return None
 
-    def initial_triage_for_viable_locations(self, center):
+    def initial_triage_for_viable_locations(self, townhall_center):
         """ Find all positions behind the mineral line"""
-        close_points = range(-11, 12)
-        close_mineral_fields = self.state.mineral_field.closer_than(10, center)
-        if close_mineral_fields:
+        surroundings = range(-11, 12)
+        townhall_mineral_fields = self.state.mineral_field.closer_than(10, townhall_center)
+        if townhall_mineral_fields:
             self.viable_points = [
                 point
                 for point in (
-                    Point2((x + center.x, y + center.y))
-                    for x in close_points
-                    for y in close_points
+                    Point2((x + townhall_center.x, y + townhall_center.y))
+                    for x in surroundings
+                    for y in surroundings
                     if 121 >= x * x + y * y >= 81
                 )
-                if abs(point.distance_to(close_mineral_fields.closest_to(point)) - 3) < 0.5
+                if abs(point.distance_to(townhall_mineral_fields.closest_to(point)) - 3) < 0.5
             ]
 
     async def prepare_building_positions(self, center):
